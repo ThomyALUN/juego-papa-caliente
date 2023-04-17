039a85d1e1284ef49fc458741ddbf0dde27064f6
jgaviri <jgaviriat@unal.edu.co>
2023-04-17 12:38:08 -0500

    Animación pasar papa

--- mostrar_sprite.py
+++ mostrar_sprite.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+
 # Inicializar pygame
 pygame.init()
 
@@ -19,10 +20,36 @@ sprite_height = 68
 def draw_sprite(row, column):
     sprite_rect = pygame.Rect(column * sprite_width, row * sprite_height, sprite_width, sprite_height)
     sprite_image = sprite_sheet.subsurface(sprite_rect)
-    screen.blit(sprite_image, (column * sprite_width, row * sprite_height)) #Copia una superficie en otra superficie
+    #crear objeto de sprite
+    sprite = pygame.sprite.Sprite()
+    sprite.image = sprite_image
+    sprite.rect = sprite_image.get_rect()
+    sprite.rect.center = (screen_width // 2, screen_height // 2)
+    #Mete el sprite al grupo de sprites
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(sprite)
+    all_sprites.draw(screen)
+    # Retorna el sprite creado
+    return sprite
 
-# Mostrar algunos sprites en la pantalla
-draw_sprite(0, 0) # primer sprite
+# Variables para llevar el seguimiento del sprite actual
+current_row = 0
+current_column = 0
+current_sprite = draw_sprite(current_row, current_column)
+
+# Configurar temporizador para volver al sprite inicial
+TIMER_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(TIMER_EVENT, 2000) # 2000 milisegundos = 2 segundos
+
+# Función para cambiar el sprite mostrado
+def change_sprite():
+    global current_row, current_column, current_sprite
+    # Incrementa la columna del sprite actual
+    current_row = 1
+    current_column = 1
+    current_sprite.kill()
+    # Muestra el nuevo sprite en la misma posición que el anterior
+    current_sprite = draw_sprite(current_row, current_column)
 
 
 # Ejecutar el ciclo del juego
@@ -32,6 +59,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
+        elif event.type == TIMER_EVENT:
+            # Volver al sprite inicial después del temporizador
+            current_row = 0
+            current_column = 0
+            current_sprite = draw_sprite(current_row, current_column)
+            # Mostrar el sprite inicial en la pantalla    
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # Cambia el sprite mostrado al presionar la tecla espacio
+                change_sprite()
 
     # Actualizar la pantalla
     pygame.display.update()
